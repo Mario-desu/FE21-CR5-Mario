f7083f1da53ccd60bd9e9e24bdba838ee2324219
Mario Hartleb <mhartleb83@yahoo.com>
2021-03-19 16:20:24 +0100

    add all instances

--- scripts/script.js
+++ scripts/script.js
@@ -23,13 +23,15 @@ var Locations = /** @class */ (function () {
         console.table(travelArray); // no instance yet, so no output
     }
     Locations.prototype.display = function () {
-        var generator = "\n        <div class=\"col\">\n         <div class=\"card h-100 shadow\">\n            <div class=\"card-body\">\n              <h5 class=\"card-title\">" + this.attraction + "</h5>\n              <p class=\"card-text\">" + this.adress + "</p>\n            </div>\n         <img src=\"" + this.image + "\" class=\"card-img-top img-fluid img-style\" alt=\"card-image\">\n         </div>\n        </div>\n       ";
+        var generator = "\n        <div class=\"col\">\n         <div class=\"card h-100 shadow\">\n            <div class=\"card-body\">\n              <h5 class=\"card-title\">" + this.attraction + "</h5>\n              <p class=\"card-text\">" + this.adress + "</p>\n            </div>\n         <img src=\"" + this.image + "\" class=\"card-img-top d-none d-sm-block img-style\" alt=\"card-image\">\n         </div>\n        </div>\n       ";
         return generator;
     };
     return Locations;
 }());
 var reichstag = new Locations("Reichstag", "Platz der Republik 1, 11011 Berlin", "img/reichstag.jpg");
 var ddrMuseum = new Locations("DDR Museum", "Karl-Liebknecht-Str. 1, 10178 Berlin", "img/ddrmuseum.jpg");
+var fernsehturm = new Locations("Berliner Fernsehturm", "Panoramastraße 1A, 10178 Berlin", "img/fernsehturm.jpg");
+var brandenburg = new Locations("Brandenburger Tor", "Pariser Platz, 10117 Berlin", "img/brandenburg.jpg");
 var Restaurant = /** @class */ (function (_super) {
     __extends(Restaurant, _super);
     function Restaurant(attraction, adress, phone, cuisine, website, image) {
@@ -40,13 +42,15 @@ var Restaurant = /** @class */ (function (_super) {
         return _this;
     }
     Restaurant.prototype.display = function () {
-        var generator = "\n        <div class=\"col\">\n          <div class=\"card h-100 shadow\">\n            <div class=\"card-body\">\n              <h5 class=\"card-title\">" + this.attraction + "</h5>\n              <p class=\"card-text\">" + this.adress + "</p>\n              <p class=\"card-text\">" + this.phone + "</p>\n              <p class=\"card-text\">" + this.cuisine + "</p>\n              <p class=\"card-text\">" + this.website + "</p>\n            </div>\n          <img src=\"" + this.image + "\" class=\"card-img-top img-fluid img-style\" alt=\"card-image\">\n          </div>\n        </div>\n        ";
+        var generator = "\n        <div class=\"col\">\n          <div class=\"card h-100 shadow\">\n            <div class=\"card-body\">\n              <h5 class=\"card-title\">" + this.attraction + "</h5>\n              <p class=\"card-text\">" + this.adress + "</p>\n              <p class=\"card-text\">" + this.phone + "</p>\n              <p class=\"card-text\">" + this.cuisine + " cuisine</p>\n              <p class=\"card-text\">" + this.website + "</p>\n            </div>\n          <img src=\"" + this.image + "\" class=\"card-img-top d-none d-sm-block img-style\" alt=\"card-image\">\n          </div>\n        </div>\n        ";
         return generator;
     };
     return Restaurant;
 }(Locations));
 var skykitchen = new Restaurant("Skykitchen", "Landsberger Allee 106, 10369 Berlin", "+49 30 4530532620", "International", "https://www.skykitchen.berlin", "img/skykitchen.jpg");
 var upperBurgerGrill = new Restaurant("Upper Burger Grill", "Rankestraße 3, 10789 Berlin", "+49 30 55221733", "American", "https://upperburgergrill.com", "img/upperburger.jpg");
+var caocao = new Restaurant("Cao Cao Restaurant", "Marburger Str. 2, 10789 Berlin", "+49 30 21915415", "Vietnamese", "https://www.cao-cao.de/", "img/caocao.jpg");
+var iimori = new Restaurant("Iimori Ramen", "Mulackstraße 29, 10119 Berlin", "+493033905188", "Japanese", "https://nipponya.de/iimori-ramen-mitte", "https://i2.wp.com/nipponya.de/wp-content/uploads/2020/07/iimori_ramen_3.jpg");
 var Events = /** @class */ (function (_super) {
     __extends(Events, _super);
     function Events(attraction, adress, eventDate, eventTime, ticketPrice, image) {
@@ -57,13 +61,15 @@ var Events = /** @class */ (function (_super) {
         return _this;
     }
     Events.prototype.display = function () {
-        var generator = "\n        <div class=\"col\">\n          <div class=\"card h-100 shadow\">\n            <div class=\"card-body\">\n              <h5 class=\"card-title\">" + this.attraction + "</h5>\n              <p class=\"card-text\">" + this.adress + "</p>\n              <p class=\"card-text\">" + this.eventDate + "</p>\n              <p class=\"card-text\">" + this.eventTime + "</p>\n              <p class=\"card-text\">" + this.ticketPrice + "</p>\n          </div>\n          <img src=\"" + this.image + "\" class=\"card-img-top img-fluid img-style\" alt=\"card-image\">\n          </div>\n        </div>        \n        ";
+        var generator = "\n        <div class=\"col\">\n          <div class=\"card h-100 shadow\">\n            <div class=\"card-body\">\n              <h5 class=\"card-title\">" + this.attraction + "</h5>\n              <p class=\"card-text\">" + this.adress + "</p>\n              <p class=\"card-text\">Date: " + this.eventDate + "</p>\n              <p class=\"card-text\">Time: " + this.eventTime + "</p>\n              <p class=\"card-text\">Price: \u20AC " + (this.ticketPrice).toFixed(2) + "</p>\n          </div>\n          <img src=\"" + this.image + "\" class=\"card-img-top d-none d-sm-block img-style\" alt=\"card-image\">\n          </div>\n        </div>        \n        ";
         return generator;
     };
     return Events;
 }(Locations));
-var evanescence = new Events("Evanescence & Within Temptation", "Velodrom,Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg");
+var evanescence = new Events("Evanescence / Within Temptation", "Velodrom,Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg");
 var maximoPark = new Events("Maximo Park", "Astra Kulturhaus, 	Revaler Straße 99, 10245 Berlin", "17.09.2021", "20:00", 34.30, "img/maximopark.jpg");
+var biffyClyro = new Events("Biffy Clyro", "Velodrom,Paul-Heyse-Straße 26, 10407 Berlin", "25.10.21", "20.00", 49.25, "img/biffyclyro.jpg");
+var deepPurple = new Events("Deep Purple", "Max-Schmeling-Halle, 	Am Falkplatz 1, 10437 Berlin", "15.10.21", "19:30", 59.75, "img/deeppurple.jpg");
 for (var _i = 0, travelArray_1 = travelArray; _i < travelArray_1.length; _i++) {
     var value = travelArray_1[_i];
     // console.log(value);
--- scripts/script.ts
+++ scripts/script.ts
@@ -22,7 +22,7 @@ class Locations {
               <h5 class="card-title">${this.attraction}</h5>
               <p class="card-text">${this.adress}</p>
             </div>
-         <img src="${this.image}" class="card-img-top img-fluid img-style" alt="card-image">
+         <img src="${this.image}" class="card-img-top d-none d-sm-block img-style" alt="card-image">
          </div>
         </div>
        `
@@ -34,7 +34,8 @@ class Locations {
 
 let reichstag = new Locations("Reichstag", "Platz der Republik 1, 11011 Berlin", "img/reichstag.jpg");
 let ddrMuseum = new Locations("DDR Museum", "Karl-Liebknecht-Str. 1, 10178 Berlin", "img/ddrmuseum.jpg");
-
+let fernsehturm = new Locations("Berliner Fernsehturm", "Panoramastraße 1A, 10178 Berlin", "img/fernsehturm.jpg")
+let brandenburg = new Locations("Brandenburger Tor", "Pariser Platz, 10117 Berlin", "img/brandenburg.jpg")
 
 
 
@@ -58,10 +59,10 @@ class Restaurant extends Locations {
               <h5 class="card-title">${this.attraction}</h5>
               <p class="card-text">${this.adress}</p>
               <p class="card-text">${this.phone}</p>
-              <p class="card-text">${this.cuisine}</p>
+              <p class="card-text">${this.cuisine} cuisine</p>
               <p class="card-text">${this.website}</p>
             </div>
-          <img src="${this.image}" class="card-img-top img-fluid img-style" alt="card-image">
+          <img src="${this.image}" class="card-img-top d-none d-sm-block img-style" alt="card-image">
           </div>
         </div>
         `
@@ -73,8 +74,8 @@ class Restaurant extends Locations {
 
 let skykitchen = new Restaurant("Skykitchen", "Landsberger Allee 106, 10369 Berlin", "+49 30 4530532620", "International", "https://www.skykitchen.berlin", "img/skykitchen.jpg");
 let upperBurgerGrill = new Restaurant("Upper Burger Grill", "Rankestraße 3, 10789 Berlin", "+49 30 55221733", "American", "https://upperburgergrill.com", "img/upperburger.jpg");
-
-
+let caocao = new Restaurant("Cao Cao Restaurant", "Marburger Str. 2, 10789 Berlin", "+49 30 21915415", "Vietnamese", "https://www.cao-cao.de/", "img/caocao.jpg" )
+let iimori = new Restaurant("Iimori Ramen", "Mulackstraße 29, 10119 Berlin", "+493033905188", "Japanese", "https://nipponya.de/iimori-ramen-mitte", "https://i2.wp.com/nipponya.de/wp-content/uploads/2020/07/iimori_ramen_3.jpg")
 
 
 class Events extends Locations {
@@ -96,11 +97,11 @@ class Events extends Locations {
             <div class="card-body">
               <h5 class="card-title">${this.attraction}</h5>
               <p class="card-text">${this.adress}</p>
-              <p class="card-text">${this.eventDate}</p>
-              <p class="card-text">${this.eventTime}</p>
-              <p class="card-text">${this.ticketPrice}</p>
+              <p class="card-text">Date: ${this.eventDate}</p>
+              <p class="card-text">Time: ${this.eventTime}</p>
+              <p class="card-text">Price: € ${(this.ticketPrice).toFixed(2)}</p>
           </div>
-          <img src="${this.image}" class="card-img-top img-fluid img-style" alt="card-image">
+          <img src="${this.image}" class="card-img-top d-none d-sm-block img-style" alt="card-image">
           </div>
         </div>        
         `
@@ -111,9 +112,10 @@ class Events extends Locations {
 }
 
 
-let evanescence = new Events("Evanescence & Within Temptation", "Velodrom,Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg");
+let evanescence = new Events("Evanescence / Within Temptation", "Velodrom,Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg");
 let maximoPark = new Events("Maximo Park", "Astra Kulturhaus, 	Revaler Straße 99, 10245 Berlin", "17.09.2021", "20:00", 34.30, "img/maximopark.jpg");
-
+let biffyClyro = new Events("Biffy Clyro", "Velodrom,Paul-Heyse-Straße 26, 10407 Berlin", "25.10.21", "20.00", 49.25, "img/biffyclyro.jpg");
+let deepPurple = new Events("Deep Purple", "Max-Schmeling-Halle, 	Am Falkplatz 1, 10437 Berlin", "15.10.21", "19:30", 59.75, "img/deeppurple.jpg")
 
 
 
