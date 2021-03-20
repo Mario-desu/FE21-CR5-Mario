a4cf7aa9f23e413dc65bc342b6e5391119d48d9b
Mario Hartleb <mhartleb83@yahoo.com>
2021-03-20 16:57:32 +0100

    last changes

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,7 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+// Location class:
 var travelArray = [];
 var Locations = /** @class */ (function () {
     function Locations(attraction, adress, image, created) {
@@ -29,15 +30,42 @@ var Locations = /** @class */ (function () {
     };
     return Locations;
 }());
-var reichstag = new Locations("Reichstag", "Platz der Republik 1, 11011 Berlin", "img/reichstag.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-var ddrMuseum = new Locations("DDR Museum", "Karl-Liebknecht-Str. 1, 10178 Berlin", "img/ddrmuseum.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-var fernsehturm = new Locations("Berliner Fernsehturm", "Panoramastraße 1A, 10178 Berlin", "img/fernsehturm.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-var brandenburg = new Locations("Brandenburger Tor", "Pariser Platz, 10117 Berlin", "img/brandenburg.jpg", new Date("Fri Dec 08 2020 07:44:57"));
+var reichstag = new Locations("Reichstag", "Platz der Republik 1, 11011 Berlin", "img/reichstag.jpg", new Date("Wed Sep 29 2021 14:25:28"));
+var ddrMuseum = new Locations("DDR Museum", "Karl-Liebknecht-Str. 1, 10178 Berlin", "img/ddrmuseum.jpg", new Date("Mon Oct 18 2021 07:44:57"));
+var fernsehturm = new Locations("Berliner Fernsehturm", "Panoramastraße 1A, 10178 Berlin", "img/fernsehturm.jpg", new Date("Thu Oct 21 2021 21:35:08"));
+var brandenburg = new Locations("Brandenburger Tor", "Pariser Platz, 10117 Berlin", "img/brandenburg.jpg", new Date("Sun Oct 10 2021 12:15:10"));
+//loop for Locations-Section, you have to put it here, otherwise it will also display the children here
 for (var _i = 0, travelArray_1 = travelArray; _i < travelArray_1.length; _i++) {
     var value = travelArray_1[_i];
-    // console.log(value);
     document.getElementById("inputLocations").innerHTML += value.display();
 }
+//Ascending-Function for Locations-Section  
+function sortingAscendLocation() {
+    document.getElementById("inputLocations").innerHTML = "";
+    travelArray.sort(function (a, b) {
+        return new Date(a.created) - new Date(b.created);
+    });
+    for (var _i = 0, travelArray_2 = travelArray; _i < travelArray_2.length; _i++) {
+        var value = travelArray_2[_i];
+        document.getElementById("inputLocations").innerHTML += value.display();
+    }
+}
+document.getElementById("ascendingLocation").addEventListener("click", sortingAscendLocation);
+//---End Ascend-Function Locations-Section----
+// Descending-Function for Locations-Section 
+function sortingDescendLocation() {
+    document.getElementById("inputLocations").innerHTML = "";
+    travelArray.sort(function (a, b) {
+        return new Date(b.created) - new Date(a.created);
+    });
+    for (var _i = 0, travelArray_3 = travelArray; _i < travelArray_3.length; _i++) {
+        var value = travelArray_3[_i];
+        document.getElementById("inputLocations").innerHTML += value.display();
+    }
+}
+document.getElementById("descendingLocation").addEventListener("click", sortingDescendLocation);
+//--end Descend-Function Locations-Section---------------   
+// Restaurant class: 
 var restaurantArray = [];
 var Restaurant = /** @class */ (function (_super) {
     __extends(Restaurant, _super);
@@ -55,15 +83,43 @@ var Restaurant = /** @class */ (function (_super) {
     };
     return Restaurant;
 }(Locations));
-var skykitchen = new Restaurant("Skykitchen", "Landsberger Allee 106, 10369 Berlin", "+49 30 4530532620", "International", "https://www.skykitchen.berlin", "img/skykitchen.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-var upperBurgerGrill = new Restaurant("Upper Burger Grill", "Rankestraße 3, 10789 Berlin", "+49 30 55221733", "American", "https://upperburgergrill.com", "img/upperburger.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-var caocao = new Restaurant("Cao Cao Restaurant", "Marburger Str. 2, 10789 Berlin", "+49 30 21915415", "Vietnamese", "https://www.cao-cao.de/", "img/caocao.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-var iimori = new Restaurant("Iimori Ramen", "Mulackstraße 29, 10119 Berlin", "+493033905188", "Japanese", "https://nipponya.de/iimori-ramen-mitte", "img/iimori.jpg", new Date("Fri Dec 08 2020 07:44:57"));
+var skykitchen = new Restaurant("Skykitchen", "Landsberger Allee 106, 10369 Berlin", "+49 30 4530532620", "International", "https://www.skykitchen.berlin", "img/skykitchen.jpg", new Date("Tue Sep 21 2021 08:34:48"));
+var upperBurgerGrill = new Restaurant("Upper Burger Grill", "Rankestraße 3, 10789 Berlin", "+49 30 55221733", "American", "https://upperburgergrill.com", "img/upperburger.jpg", new Date("Fri Oct 08 2021 07:08:57"));
+var caocao = new Restaurant("Cao Cao Restaurant", "Marburger Str. 2, 10789 Berlin", "+49 30 21915415", "Vietnamese", "https://www.cao-cao.de/", "img/caocao.jpg", new Date("Fri Oct 15 2021 11:11:22"));
+var iimori = new Restaurant("Iimori Ramen", "Mulackstraße 29, 10119 Berlin", "+493033905188", "Japanese", "https://nipponya.de/iimori-ramen-mitte", "img/iimori.jpg", new Date("Fri Oct 22 2021 01:22:11"));
+//------Loop for restaurant section----------------
 for (var _a = 0, restaurantArray_1 = restaurantArray; _a < restaurantArray_1.length; _a++) {
     var value = restaurantArray_1[_a];
     // console.log(value);
     document.getElementById("inputRestaurant").innerHTML += value.display();
 }
+//Ascending-Function for Restaurant-Section  
+function sortingAscendRest() {
+    document.getElementById("inputRestaurant").innerHTML = "";
+    restaurantArray.sort(function (a, b) {
+        return new Date(a.created) - new Date(b.created);
+    });
+    for (var _i = 0, restaurantArray_2 = restaurantArray; _i < restaurantArray_2.length; _i++) {
+        var value = restaurantArray_2[_i];
+        document.getElementById("inputRestaurant").innerHTML += value.display();
+    }
+}
+document.getElementById("ascendingRest").addEventListener("click", sortingAscendRest);
+//---End Ascend-Function Restaurant----
+// Descending-Function for Restaurant-Section  
+function sortingDescendRest() {
+    document.getElementById("inputRestaurant").innerHTML = "";
+    restaurantArray.sort(function (a, b) {
+        return new Date(b.created) - new Date(a.created);
+    });
+    for (var _i = 0, restaurantArray_3 = restaurantArray; _i < restaurantArray_3.length; _i++) {
+        var value = restaurantArray_3[_i];
+        document.getElementById("inputRestaurant").innerHTML += value.display();
+    }
+}
+document.getElementById("descendingRest").addEventListener("click", sortingDescendRest);
+//--end Descend-Function Restaurant---------------   
+// Event class
 var eventsArray = [];
 var Events = /** @class */ (function (_super) {
     __extends(Events, _super);
@@ -82,20 +138,41 @@ var Events = /** @class */ (function (_super) {
     };
     return Events;
 }(Locations));
-var evanescence = new Events("Evanescence / Within Temptation", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg", "www.evanescence.com", new Date("Fri Sep 19 2021 07:44:57"));
-var maximoPark = new Events("Maximo Park", "Astra Kulturhaus - Revaler Straße 99, 10245 Berlin", "17.09.2021", "20:00", 34.30, "img/maximopark.jpg", "www.maximopark.com", new Date("Fri Oct 18 2021 07:44:57"));
-var biffyClyro = new Events("Biffy Clyro", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "25.10.21", "20.00", 49.25, "img/biffyclyro.jpg", "www.biffyclyro.com", new Date("Fri Jun 08 2021 07:44:57"));
-var deepPurple = new Events("Deep Purple", "Max-Schmeling-Halle - Am Falkplatz 1, 10437 Berlin", "15.10.21", "19:30", 59.75, "img/deeppurple.jpg", "https://deeppurple.com", new Date("Fri Dec 08 2020 07:44:57"));
+var evanescence = new Events("Evanescence / Within Temptation", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg", "www.evanescence.com", new Date("Mon Sep 13 2021 06:44:57"));
+var maximoPark = new Events("Maximo Park", "Astra Kulturhaus - Revaler Straße 99, 10245 Berlin", "17.09.2021", "20:00", 34.30, "img/maximopark.jpg", "www.maximopark.com", new Date("Sat Sep 18 2021 07:56:47"));
+var biffyClyro = new Events("Biffy Clyro", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "25.10.21", "20.00", 49.25, "img/biffyclyro.jpg", "www.biffyclyro.com", new Date("Thu Oct 26 2021 03:33:52"));
+var deepPurple = new Events("Deep Purple", "Max-Schmeling-Halle - Am Falkplatz 1, 10437 Berlin", "15.10.21", "19:30", 59.75, "img/deeppurple.jpg", "https://deeppurple.com", new Date("Sat Oct 16 2021 11:24:57"));
+// ----Loop to for Event-Section-------------
 for (var _b = 0, eventsArray_1 = eventsArray; _b < eventsArray_1.length; _b++) {
     var value = eventsArray_1[_b];
     // console.log(value);
     document.getElementById("inputEvents").innerHTML += value.display();
 }
-function sortingDescend() {
+//Ascending-Function for Event-Section  
+function sortingAscendEvent() {
+    document.getElementById("inputEvents").innerHTML = "";
     eventsArray.sort(function (a, b) {
-        // Turn your strings into dates, and then subtract them
-        // to get a value that is either negative, positive, or zero.
+        return new Date(a.created) - new Date(b.created);
+    });
+    console.log(eventsArray);
+    for (var _i = 0, eventsArray_2 = eventsArray; _i < eventsArray_2.length; _i++) {
+        var value = eventsArray_2[_i];
+        document.getElementById("inputEvents").innerHTML += value.display();
+    }
+}
+document.getElementById("ascendingEvent").addEventListener("click", sortingAscendEvent);
+//---End Ascend-Function Event----
+// Descending-Function for Event-Section  
+function sortingDescendEvent() {
+    document.getElementById("inputEvents").innerHTML = "";
+    eventsArray.sort(function (a, b) {
         return new Date(b.created) - new Date(a.created);
     });
     console.log(eventsArray);
+    for (var _i = 0, eventsArray_3 = eventsArray; _i < eventsArray_3.length; _i++) {
+        var value = eventsArray_3[_i];
+        document.getElementById("inputEvents").innerHTML += value.display();
+    }
 }
+document.getElementById("descendingEvent").addEventListener("click", sortingDescendEvent);
+//--end Descend-Function Event---------------   
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,4 +1,4 @@
-
+// Location class:
 let travelArray:Array<Locations> = [];
 
 class Locations {
@@ -36,18 +36,86 @@ class Locations {
 
 }
 
-let reichstag = new Locations("Reichstag", "Platz der Republik 1, 11011 Berlin", "img/reichstag.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-let ddrMuseum = new Locations("DDR Museum", "Karl-Liebknecht-Str. 1, 10178 Berlin", "img/ddrmuseum.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-let fernsehturm = new Locations("Berliner Fernsehturm", "Panoramastraße 1A, 10178 Berlin", "img/fernsehturm.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-let brandenburg = new Locations("Brandenburger Tor", "Pariser Platz, 10117 Berlin", "img/brandenburg.jpg", new Date("Fri Dec 08 2020 07:44:57"))
+let reichstag = new Locations("Reichstag", "Platz der Republik 1, 11011 Berlin", "img/reichstag.jpg", new Date("Wed Sep 29 2021 14:25:28"));
+let ddrMuseum = new Locations("DDR Museum", "Karl-Liebknecht-Str. 1, 10178 Berlin", "img/ddrmuseum.jpg", new Date("Mon Oct 18 2021 07:44:57"));
+let fernsehturm = new Locations("Berliner Fernsehturm", "Panoramastraße 1A, 10178 Berlin", "img/fernsehturm.jpg", new Date("Thu Oct 21 2021 21:35:08"));
+let brandenburg = new Locations("Brandenburger Tor", "Pariser Platz, 10117 Berlin", "img/brandenburg.jpg", new Date("Sun Oct 10 2021 12:15:10"))
+
+
+
+//loop for Locations-Section, you have to put it here, otherwise it will also display the children here
 
 for (let value of travelArray){
 
-  // console.log(value);
+ 
   document.getElementById("inputLocations").innerHTML += value.display();
   
 }
 
+
+
+//Ascending-Function for Locations-Section  
+
+function sortingAscendLocation(){
+  document.getElementById("inputLocations").innerHTML ="";
+
+  travelArray.sort(function(a,b){
+
+  return new Date(a.created) - new Date(b.created);
+});
+
+  for (let value of travelArray){
+
+
+  document.getElementById("inputLocations").innerHTML += value.display();
+  
+  }
+
+}
+
+document.getElementById("ascendingLocation").addEventListener("click", sortingAscendLocation);
+
+
+  //---End Ascend-Function Locations-Section----
+
+
+
+// Descending-Function for Locations-Section 
+
+function sortingDescendLocation(){
+  document.getElementById("inputLocations").innerHTML ="";
+
+  travelArray.sort(function(a,b){
+
+  return new Date(b.created) - new Date(a.created);
+});
+
+
+
+for (let value of travelArray){
+
+
+  document.getElementById("inputLocations").innerHTML += value.display();
+  
+  }
+
+}
+
+document.getElementById("descendingLocation").addEventListener("click", sortingDescendLocation);
+
+//--end Descend-Function Locations-Section---------------   
+
+
+
+
+
+
+
+
+
+
+
+// Restaurant class: 
 
 
 let restaurantArray:Array<Restaurant> = [];
@@ -89,11 +157,14 @@ class Restaurant extends Locations {
 }
 
 
-let skykitchen = new Restaurant("Skykitchen", "Landsberger Allee 106, 10369 Berlin", "+49 30 4530532620", "International", "https://www.skykitchen.berlin", "img/skykitchen.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-let upperBurgerGrill = new Restaurant("Upper Burger Grill", "Rankestraße 3, 10789 Berlin", "+49 30 55221733", "American", "https://upperburgergrill.com", "img/upperburger.jpg", new Date("Fri Dec 08 2020 07:44:57"));
-let caocao = new Restaurant("Cao Cao Restaurant", "Marburger Str. 2, 10789 Berlin", "+49 30 21915415", "Vietnamese", "https://www.cao-cao.de/", "img/caocao.jpg", new Date("Fri Dec 08 2020 07:44:57"))
-let iimori = new Restaurant("Iimori Ramen", "Mulackstraße 29, 10119 Berlin", "+493033905188", "Japanese", "https://nipponya.de/iimori-ramen-mitte", "img/iimori.jpg", new Date("Fri Dec 08 2020 07:44:57"))
+let skykitchen = new Restaurant("Skykitchen", "Landsberger Allee 106, 10369 Berlin", "+49 30 4530532620", "International", "https://www.skykitchen.berlin", "img/skykitchen.jpg", new Date("Tue Sep 21 2021 08:34:48"));
+let upperBurgerGrill = new Restaurant("Upper Burger Grill", "Rankestraße 3, 10789 Berlin", "+49 30 55221733", "American", "https://upperburgergrill.com", "img/upperburger.jpg", new Date("Fri Oct 08 2021 07:08:57"));
+let caocao = new Restaurant("Cao Cao Restaurant", "Marburger Str. 2, 10789 Berlin", "+49 30 21915415", "Vietnamese", "https://www.cao-cao.de/", "img/caocao.jpg", new Date("Fri Oct 15 2021 11:11:22"))
+let iimori = new Restaurant("Iimori Ramen", "Mulackstraße 29, 10119 Berlin", "+493033905188", "Japanese", "https://nipponya.de/iimori-ramen-mitte", "img/iimori.jpg", new Date("Fri Oct 22 2021 01:22:11"))
 
+
+
+//------Loop for restaurant section----------------
 
 for (let value of restaurantArray){
 
@@ -102,7 +173,69 @@ for (let value of restaurantArray){
   
 }
 
+//Ascending-Function for Restaurant-Section  
 
+function sortingAscendRest(){
+  document.getElementById("inputRestaurant").innerHTML ="";
+
+  restaurantArray.sort(function(a,b){
+
+  return new Date(a.created) - new Date(b.created);
+});
+
+
+for (let value of restaurantArray){
+
+
+  document.getElementById("inputRestaurant").innerHTML += value.display();
+  
+  }
+
+}
+
+document.getElementById("ascendingRest").addEventListener("click", sortingAscendRest);
+
+
+  //---End Ascend-Function Restaurant----
+
+
+
+// Descending-Function for Restaurant-Section  
+
+function sortingDescendRest(){
+  document.getElementById("inputRestaurant").innerHTML ="";
+
+  restaurantArray.sort(function(a,b){
+
+  return new Date(b.created) - new Date(a.created);
+});
+
+
+
+for (let value of restaurantArray){
+
+
+  document.getElementById("inputRestaurant").innerHTML += value.display();
+  
+  }
+
+}
+
+document.getElementById("descendingRest").addEventListener("click", sortingDescendRest);
+
+//--end Descend-Function Restaurant---------------   
+
+
+
+
+
+
+
+
+
+
+
+// Event class
 
 
 let eventsArray:Array<Events> = [];
@@ -148,18 +281,13 @@ class Events extends Locations {
 }
 
 
-let evanescence = new Events("Evanescence / Within Temptation", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg", "www.evanescence.com", new Date("Fri Sep 19 2021 07:44:57"));
-let maximoPark = new Events("Maximo Park", "Astra Kulturhaus - Revaler Straße 99, 10245 Berlin", "17.09.2021", "20:00", 34.30, "img/maximopark.jpg", "www.maximopark.com", new Date("Fri Oct 18 2021 07:44:57"));
-let biffyClyro = new Events("Biffy Clyro", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "25.10.21", "20.00", 49.25, "img/biffyclyro.jpg", "www.biffyclyro.com", new Date("Fri Jun 08 2021 07:44:57"));
-let deepPurple = new Events("Deep Purple", "Max-Schmeling-Halle - Am Falkplatz 1, 10437 Berlin", "15.10.21", "19:30", 59.75, "img/deeppurple.jpg", "https://deeppurple.com", new Date("Fri Dec 08 2020 07:44:57"));
+let evanescence = new Events("Evanescence / Within Temptation", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "11.09.2021", "19:00", 67.90, "img/evanescence.jpg", "www.evanescence.com", new Date("Mon Sep 13 2021 06:44:57"));
+let maximoPark = new Events("Maximo Park", "Astra Kulturhaus - Revaler Straße 99, 10245 Berlin", "17.09.2021", "20:00", 34.30, "img/maximopark.jpg", "www.maximopark.com", new Date("Sat Sep 18 2021 07:56:47"));
+let biffyClyro = new Events("Biffy Clyro", "Velodrom - Paul-Heyse-Straße 26, 10407 Berlin", "25.10.21", "20.00", 49.25, "img/biffyclyro.jpg", "www.biffyclyro.com", new Date("Thu Oct 26 2021 03:33:52"));
+let deepPurple = new Events("Deep Purple", "Max-Schmeling-Halle - Am Falkplatz 1, 10437 Berlin", "15.10.21", "19:30", 59.75, "img/deeppurple.jpg", "https://deeppurple.com", new Date("Sat Oct 16 2021 11:24:57"));
 
 
-
-
-
-
-
-
+// ----Loop to for Event-Section-------------
 
   for (let value of eventsArray){
 
@@ -169,18 +297,59 @@ let deepPurple = new Events("Deep Purple", "Max-Schmeling-Halle - Am Falkplatz 1
   }
 
 
-function sortingDescend():void{
-  
+
+
+
+//Ascending-Function for Event-Section  
+
+function sortingAscendEvent(){
+  document.getElementById("inputEvents").innerHTML ="";
+
   eventsArray.sort(function(a,b){
-  // Turn your strings into dates, and then subtract them
-  // to get a value that is either negative, positive, or zero.
-  return new Date(b.created) - new Date(a.created);
+
+  return new Date(a.created) - new Date(b.created);
 });
 console.log(eventsArray);
-eventsArray
+
+
+for (let value of eventsArray){
+
+
+  document.getElementById("inputEvents").innerHTML += value.display();
+  
+  }
 
 }
 
+document.getElementById("ascendingEvent").addEventListener("click", sortingAscendEvent);
 
-   
+
+  //---End Ascend-Function Event----
+
+
+
+// Descending-Function for Event-Section  
+
+function sortingDescendEvent(){
+  document.getElementById("inputEvents").innerHTML ="";
+
+  eventsArray.sort(function(a,b){
+
+  return new Date(b.created) - new Date(a.created);
+});
+console.log(eventsArray);
+
+
+for (let value of eventsArray){
+
+
+  document.getElementById("inputEvents").innerHTML += value.display();
+  
+  }
+
+}
+
+document.getElementById("descendingEvent").addEventListener("click", sortingDescendEvent);
+
+//--end Descend-Function Event---------------   
 
